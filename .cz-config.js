fcdf9680d10af463116e0fa0e9e8104d02eff9c3
João Victor Negreiros <joaovictornsv@gmail.com>
2021-01-28 17:41:12 -0300

    :house:  dsds

--- .cz-config.js
+++ .cz-config.js
@@ -3,47 +3,47 @@
 module.exports = {
   types: [
     {
-      value: ":house: (build)",
+      value: ":house:(build)",
       name: "➕ (build):\tFix CI Build." 
     },
     { 
-      value: ":gear: (chore)",
+      value: ":gear:(chore)",
       name: "⚙️  (chore):\t Dependencies configs (add, remove or update)."
     },
     { 
-      value: ":construction_worker: (ci)",
+      value: ":construction_worker:(ci)",
       name: "⚠️  (ci):\t Add or update CI build system."
     },
     { 
-      value: ":memo: (docs)",
+      value: ":memo:(docs)",
       name: "✏️  (docs):\t Add or update documentation."
     },
     { 
-      value: ":sparkles: (feat)",
+      value: ":sparkles:(feat)",
       name: "✨ (feat):\tIntroduce new features."
     },
     { 
-      value: ":bug: (fix)",
+      value: ":bug:(fix)",
       name: "✔️  (fix):\t Fix a bug."
     },
     { 
-      value: ":zap: (perf)",
+      value: ":zap:(perf)",
       name: "⚡️ (perf):\t Improve performance."
     },
     { 
-      value: ":recycle: (refactor)",
+      value: ":recycle:(refactor)",
       name: "♻️  (refact):\t Refactor code."
     },
     { 
-      value: ":rewind: (revert)",
+      value: ":rewind:(revert)",
       name: "⏪ (revert):\tRevert changes."
     },
     { 
-      value: ":art: (style)",
+      value: ":art:(style)",
       name: "⭐️ (style):\t Add or update the UI and style files."
     },
     { 
-      value: ":white_check_mark: (test)",
+      value: ":white_check_mark:(test)",
       name: "✅ (test):\tAdd or update tests"
     },
   ],

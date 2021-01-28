8157ddee807840b6f98a2d88f72aad8e2937b84f
João Victor Negreiros <joaovictornsv@gmail.com>
2021-01-28 13:05:48 -0300

    :gear: (chore): config commitlint and commitizen

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  extends: ['gitmoji'],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        ':house: (build)',
+        ':gear: (chore)',
+        ':construction_worker: (ci)',
+        ':memo: (docs)',
+        ':sparkles: (feat)',
+        ':bug: (fix)',
+        ':zap: (perf)',
+        ':recycle: (refactor)',
+        ':rewind: (revert)',
+        ':art: (style)',
+        ':white_check_mark: (test)',
+      ],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-max-length': [2, 'always', 50],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 72],
+  },
 };

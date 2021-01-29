8d52c628bf126e0329535d577a9c8de7f40c1d70
João Victor Negreiros <joaovictornsv@gmail.com>
2021-01-28 22:20:30 -0300

    :rewind:(revert): set default custom (cz-customizable)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  extends: ['gitmoji'],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
-      headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
-    },
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        ':house: (build)',
+        ':gear: (chore)',
+        ':construction_worker: (ci)',
+        ':memo: (docs)',
+        ':sparkles: (feat)',
+        ':bug: (fix)',
+        ':zap: (perf)',
+        ':recycle: (refactor)',
+        ':rewind: (revert)',
+        ':art: (style)',
+        ':white_check_mark: (test)',
+      ],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-max-length': [2, 'always', 50],
+    // 'subject-empty': [2, 'always'],
+    'subject-full-stop': [2, 'never', '.'],
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 72],
   },
 };

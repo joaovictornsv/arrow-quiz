15edb689bd72be7127549401ae33d1cbf78c7aaa
João Victor Negreiros <joaovictornsv@gmail.com>
2021-01-31 14:30:56 -0300

    teste lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
     'subject-case': [2, 'always', 'sentence-case'],
     'subject-max-length': [2, 'always', 50],
-    'subject-empty': [2, 'never'],
+    'subject-empty': [2, 'always'],
     'subject-full-stop': [2, 'never', '.'],
     'body-leading-blank': [2, 'always'],
     'body-max-line-length': [2, 'always', 72],

af430e96350f63f7fc3ec243ac875c6de8541526
João Victor Negreiros <joaovictornsv@gmail.com>
2021-01-31 21:23:40 -0300

    chore: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,31 +1,3 @@
 module.exports = {
-  rules: {
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        ':house:(build)',
-        ':wrench:(chore)',
-        ':construction_worker:(ci)',
-        ':memo:(docs)',
-        ':sparkles:(feat)',
-        ':bug:(fix)',
-        ':zap:(perf)',
-        ':recycle:(refactor)',
-        ':rewind:(revert)',
-        ':art:(style)',
-        ':white_check_mark:(test)',
-      ],
-    ],
-    'subject-case': [2, 'always', 'lower-case'],
-    'subject-max-length': [2, 'always', 50],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [2, 'always', 72],
-    'footer-leading-blank': [2, 'always'],
-    'footer-max-line-length': [2, 'always', 72],
-  },
+  extends: ['@commitlint/config-conventional'],
 };
